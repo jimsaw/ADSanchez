f342dc0a6d78a75ffb273eac4c7e6605d3c475ab
Luis <lfmonten@espol.edu.ec>
2021-07-15 00:27:16 -0500

    configuracion heroku app v2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-nm
 app.use(express.static('./dist/adsanchez'));
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: './dist/adsanchez/' }
